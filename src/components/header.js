557765d4dbd1fc738ed10869bc3f1639968a12c4
inoue-makoto <inm0211@gmail.com>
2021-12-18 14:38:30 +0900

    add bootstrap

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { Container, Navbar, Nav } from 'react-bootstrap'
 
 const Header = ({ siteTitle }) => (
   <header
