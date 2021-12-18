5e3e14e7f7b9aa82269c277404fc202beac397d7
inoue-makoto <inm0211@gmail.com>
2021-12-18 14:29:59 +0900

    change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `夫婦問題攻略サイト`,
+    description: `This is a website that helps couples attack the various problems they face.`,
+    author: `@夫婦問題研究所(仮)`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
